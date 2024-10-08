536ef4cf754a1098f9f4e9b7b65e9a5c6454113c
Evan <esnowgol@emich.edu>
2024-10-07 21:16:44 -0400

    Week 4

--- backend/processImage.py
+++ backend/processImage.py
@@ -1,78 +1,10 @@
+from torch import rand
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
+import random
 
-def grayScale(img):
-    #Read the image
-    #convert is to grayscale
-    im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return im_gray
-
-def getThreshhold(im_gray):
-    #find the threshhold. Anything that's less than X intensity set to 0, more than X set to 255
-    ret, im_th = cv2.threshold(im_gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    return ret, im_th
-
-def createBoundingByLetter(im_th, gap_factor=2.0):
-    ctrs, hier = cv2.findContours(im_th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    bboxes = [cv2.boundingRect(c) for c in ctrs]
-
-    # Filter out bounding boxes with width >= 10 and height >= 10
-    filtered_bboxes = [b for b in bboxes if b[2] >= 10 and b[3] >= 10]
-
-    # Sort the filtered bounding boxes by their y and x coordinates
-    sorted_bboxes = sorted(filtered_bboxes, key=lambda b: (b[1], b[0]))
-
-
-     # Compute average spacing between adjacent bounding boxes in the same row
-    average_spacing = 0
-    num_distances = 0
-    for i in range(1, len(sorted_bboxes)):
-        if sorted_bboxes[i][1] == sorted_bboxes[i-1][1]:  # Same row
-            average_spacing += sorted_bboxes[i][0] - (sorted_bboxes[i-1][0] + sorted_bboxes[i-1][2])  # Gap between boxes
-            num_distances += 1
-
-    if num_distances > 0:
-        average_spacing /= num_distances
-    
-    # Define the threshold for a new section based on the average spacing
-    threshold = gap_factor * average_spacing if average_spacing > 0 else 0
-
-    # Group bounding boxes into sections
-    sections = []
-    current_section = []
-    for i, bbox in enumerate(sorted_bboxes):
-        if i == 0:
-            current_section.append(bbox)
-        else:
-            prev_bbox = sorted_bboxes[i - 1]
-            if (bbox[1] == prev_bbox[1] and  # Same row
-                bbox[0] - (prev_bbox[0] + prev_bbox[2]) > threshold):  # Gap exceeds threshold
-                # Start a new section
-                sections.append(current_section)
-                current_section = []
-            current_section.append(bbox)
-    
-    if current_section:
-        sections.append(current_section)
-
-    return sorted_bboxes
-
-#def draw_sections(img, sections):
-    # Loop through each section (list of bounding boxes)
-    #for section in sections:
-        # Loop through each bounding box in the section
-        #for (x, y, w, h) in section:
-            # Draw a rectangle around the bounding box (green color, thickness of 2)
-            #cv2.rectangle(img, (x, y), (x + w, y + h), (24, 144, 225), 2)
-    #return img
-
-#def draw_bounding_boxes(img, bboxes):
-    # Loop through each bounding box
-    #for (x, y, w, h) in bboxes:
-        # Draw a rectangle over the image (green color, thickness of 2)
-        #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    #return img
+from backend.customDataSet import CustomImageDataset
 
 
 transform = transforms.Compose([
@@ -81,27 +13,178 @@ transform = transforms.Compose([
 	transforms.Normalize((0.1307,), (0.3081,))
 ])
 
+def createRandomKernel(channels, size):
+    kernel = []
+    for h in range(channels):
+        kernel.add([])
+        for i in range(size):
+            kernel[h].add([])
+            for j in range(size):
+                kernel[h][i].add(random.uniform(0, 1))
+    return kernel
 
-path = "C:\\Users\\evans\\Desktop\\Semester Projects\\NetGrowth\\backend\\training_data\\test_images\\test_images\\1f2c589e49a3bcd0.jpg"
+    #create Kernel of sizexsize with random values
+    #rand(size)
 
-train_dataset = ImageFolder(root='./data', train=True, download=True, transform=transform)
-test_dataset = ImageFolder(root='./data', train=False, download=True, transform=transform)
 
-batch_size = 64
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+def addPaddingToMatrix(matrix, paddingSize):
+    length = len(matrix[0])-1
+
+    #insert new row at 0
+    matrix.insert(0, [])
+
+    #for each column, add a 0
+    for i in range(length):
+        matrix[0].add(0)
+
+    #add new row at bottom
+    matrix.add([])
+
+    #for each column add a 0
+    for i in range(length):
+        matrix[len(matrix)-1].add(0)
+
+    #for all the middle rows, add a 0 at the beginning and end
+    for i in range(1, len(matrix)-1):
+        matrix.insert(0,0)
+        matrix.add(0)
+
+    return matrix
+
+def convertImageToTensor(img): #Tensor is essentially an array of Matrices. since using greyscale, it's only 1 matrix stored in the array
+    
+    #convert an image to a tensor, return resulting tensor
+    print("foo")
+
+def elementWiseMultiplication(inputMatrix, targetRow, targetColumn, kernelMatrix, outputLayer, outputRow, outputColumn):
+    #elementWise multiply around the target in the matrix, using the kernel, insert into the outputLayer at outputRow, outputColumn
+    print("foo")
+
+def buildOutputLayer(image, kernel, padding, stride, channels):
+    output = []
+    rowsOfMatrix = len(image[h])
+    columnsOfMatrix = len(image[h][0])
+    rowsOfKernel = len(kernel[h])
+    columnsOfKernel = len(kernel[h][0])
+    outputRows = (rowsOfMatrix -  rowsOfKernel + (2*padding))/stride
+    outputColumns = (columnsOfMatrix - columnsOfKernel + (2*padding))/stride
+    
+    for h in range(channels):
+        currentRow = -1
+        currentColumn = -1
+
+        totalOutputLayer= []
+        outputLayer = [] #Tensor that is outputRows X outputColumns
+        for i in range(padding, outputRows, stride):
+            currentRow += 1
+            outputLayer.add([])
+            for j in range(padding, outputColumns, stride):
+                currentColumn+=1
+                outputLayer[i].add(0)
+                #loop through each item in our tensor, call elementWiseMultiplication for each item, setting outputLayer
+                outputLayer = elementWiseMultiplication(image[h], i, j, kernel[h], outputLayer, currentRow, currentColumn)
+                if len(totalOutputLayer = 0):
+                    totalOutputLayer = outputLayer
+                else:
+                    for row in outputLayer:
+                        for column in row:
+                            totalOutputLayer[row][column] = totalOutputLayer[row][column] + outputLayer[row][column]
+    #output.add(outputLayer)
+    return output
+
+    
+def buildKernels(channels, kernels, kernelCount, kernelSize):
+    for i in range(kernelCount):
+        kernels.add(createRandomKernel(channels, kernelSize))
+    return kernels
+
+def ReLu(matrix):
+    rowsOfTensor = len(matrix)
+    columnsOfTensor = len(matrix[0])
+    for i in range(0, rowsOfTensor):
+        for j in range(0, columnsOfTensor):
+            matrix[i][j] = max(0, matrix[i][j])
+    return matrix
+
+def MaxPoolTensor(matrix, size):
+    rowsOfTensor = len(matrix)
+    columnsOfTensor = len(matrix[0])
+    outputTensor = "" #Tensor of size Ceil((rowsOfTensor/size)) x Ciel((columnsOfTensor/size)))
+    currentRow = -1
+    currentColumn = -1
+    for i in range(0, rowsOfTensor, size):
+        currentRow+=1
+        for j in range(0, columnsOfTensor, size):
+            currentColumn+=1
+            outputTensor[currentRow][currentColumn] = max(0, matrix[i][j])
+    return matrix
+
+def main():
+    train_dataset = CustomImageDataset(img_dir='./training_data/train', transform=transform)
+    test_dataset = CustomImageDataset(img_dir='./training_data/test', transform=transform)
+    padding = 1
+    stride = 1
+    pooling = 2
+    channels = 1
+    num_epochs = 10
+    convolutional_layers = 2
+    batch_size = 64
+    kernel_count = 8
+    kernels = []
+    kernels = buildKernels(channels, kernels, kernel_count, 3)
+
+
+
+    tensor = []
+
+        #startLoop
+    for epoch in num_epochs:
+        for batch in batch_size:
+            tensor = tensor.add(convertImageToTensor(train_dataset.__getitem__(random(0, len(train_dataset.image_filenames)-1))))
+
+        for channel in tensor:
+            for matrix in channel:
+                matrix = addPaddingToMatrix(matrix, padding)
+
+
+
+
+        featureMaps=[]
+        for layer in convolutional_layers:
+            for kernel in kernels:
+                for image in tensor:
+                    featureMaps.add(buildOutputLayer(image, kernel, padding, stride, channels))
+            kernels = buildKernels(channels, kernels, kernel_count, 3)
+            kernel_count = kernel_count*2
+            
+
+
+        for featureMap in featureMaps:
+            featureMap = MaxPoolTensor(featureMap, pooling)
+            featureMap = ReLu(featureMap)
+    
+        tensor = featureMaps
+
+
+    print("foo")
+
+#path = "C:\\Users\\evans\\Desktop\\Semester Projects\\NetGrowth\\backend\\training_data\\test_images\\test_images\\1f2c589e49a3bcd0.jpg"
+
+#batch_size = 64
+#train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size,shuffle=True)
+#test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 
 
 #img = cv2.imread(path)
-greyScaled = grayScale(img)
-ret, im_th = getThreshhold(greyScaled)
-sections = createBoundingByLetter(im_th, 2)
-bounded = draw_bounding_boxes(img, sections)
+#greyScaled = grayScale(img)
+#ret, im_th = getThreshhold(greyScaled)
+#sections = createBoundingByLetter(im_th, 2)
+#bounded = draw_bounding_boxes(img, sections)
 
-cv2.imshow("image window", img)
+#cv2.imshow("image window", img)
 # Wait indefinitely until a key is pressed
-cv2.waitKey(0)
+#cv2.waitKey(0)
 
 # Close all OpenCV windows when any key is pressed
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
